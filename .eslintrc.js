ae2bd98a3714b7c36bdffb13fd2ce0da25c47977
Latif Sulistyo <latifsulistyo.me@gmail.com>
2021-06-15 16:55:16 +0700

    feat: setup e2e testing 🧪

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   extends: ['ruppy-node', 'ruppy-ts', 'ruppy-jest'],
+  overrides: [
+    {
+      files: ['**/e2e/**/*'],
+      extends: ['plugin:jest-playwright/recommended'],
+    },
+  ],
   env: { serviceworker: true },
   rules: {
     'import/no-cycle': ['error', { maxDepth: 3 }],

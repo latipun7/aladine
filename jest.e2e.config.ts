3603708109dd48d1049ceaa191fca85b5517e613
Latif Sulistyo <latifsulistyo.me@gmail.com>
2021-06-16 22:42:57 +0700

    feat: add e2e tests 🧪

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -5,30 +5,17 @@ import config from './jest.config';
 
 interface E2EConfig
   extends Omit<InitialOptionsTsJest, 'testEnvironmentOptions'> {
-  testEnvironmentOptions: { 'jest-playwright': JestPlaywrightConfig };
+  testEnvironmentOptions?: { 'jest-playwright'?: JestPlaywrightConfig };
 }
 
-const { testEnvironment, ...restConfig } = config;
+const { testEnvironment, setupFiles, setupFilesAfterEnv, ...restConfig } =
+  config;
 
 const e2eConfig: E2EConfig = {
   ...restConfig,
-  preset: 'jest-playwright-jsdom',
+  preset: 'jest-playwright-preset',
   testMatch: ['**/e2e/**/*.[jt]s?(x)'],
-  testEnvironmentOptions: {
-    'jest-playwright': {
-      browsers: ['chromium', 'firefox', 'webkit'],
-      serverOptions: {
-        command: 'yarn dev',
-        port: 8080,
-        usedPortAction: 'ask',
-      },
-    },
-  },
-  setupFilesAfterEnv: [
-    ...(config.setupFilesAfterEnv ? config.setupFilesAfterEnv : []),
-    'expect-playwright',
-    'jest-playwright-preset/lib/extends.js',
-  ],
+  testTimeout: 60_000,
 };
 
 export default e2eConfig;
